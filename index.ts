d7f21843f8797e8d3262f5cd61366f97b0e0b53b
Funayd <105151651+funaydmc@users.noreply.github.com>
2025-12-03 00:05:01 +0700

    feat: Initialize a Minecraft server using flying-squid and configure project dependencies.

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log("Hello World"); 
+const mcServer = require('flying-squid')
+
+mcServer.createMCServer({
+  'motd': 'A Minecraft Server \nRunning flying-squid',
+  'port': 25080,
+  'max-players': 10,
+  'online-mode': true,
+  'logging': true,
+  'gameMode': 1,
+  'generation': {
+    'name': 'diamond_square',
+    'options': {
+      'worldHeight': 80
+    }
+  },
+  'kickTimeout': 10000,
+  'plugins': {
+
+  },
+  'modpe': false,
+  'view-distance': 10
+})
+
+console.log('Server listening on port 25565');
+
